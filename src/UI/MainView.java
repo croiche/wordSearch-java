8aa6b302b5cec43aed9aafa12c0e162562676a3a
Martin Rasmussen <martinrasmussen92@gmail.com>
2013-02-19 10:25:53 +0100

    Added some panels to the ui

--- src/UI/MainView.java
+++ src/UI/MainView.java
@@ -4,10 +4,35 @@
  */
 package UI;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Martin
  */
-public class MainView {
-    
+public class MainView extends JFrame {
+
+    private JLabel main;
+
+    public MainView() {
+        init();
+
+        this.setSize(500, 600);
+        this.setTitle("Word Searcher");
+
+
+        main = getBorderLayout();
+        this.add(main);
+
+
+    }
+
+    private void init() {
+    }
+
+    private JLabel getBorderLayout() {
+        JLabel l = new JLabel();
+        return l;
+    }
 }
