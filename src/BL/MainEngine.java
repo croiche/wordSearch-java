0df37aedef9fea5b4f13bc262c18d18fe05bbdab
Anthony Monori <anthonymonori@gmail.com>
2013-02-19 10:58:53 +0100

    Added look&feel, BL

---
+++ src/BL/MainEngine.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author anthony
+ */
+public class MainEngine {
+    
+}
--- src/UI/MainView.java
+++ src/UI/MainView.java
@@ -28,6 +28,21 @@ public class MainView extends JFrame {
 
         main = getBorderLayout();
         this.add(main);
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
 
 
     }
