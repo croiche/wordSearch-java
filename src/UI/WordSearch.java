f279a4580aa37bfe0fbda82f7d24272c34f3f5f1
Martin Rasmussen <martinrasmussen92@gmail.com>
2013-02-19 09:56:58 +0100

    Added the ui classes

---
+++ src/UI/WordSearch.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Martin
+ */
+public class WordSearch {
+    public static void main(String[] args) {
+        JFrame myFrame = new MainView();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+    }
+}
