ccc223a064e5aaf9597430766836d38218bb4fe3
Martin Rasmussen <martinrasmussen92@gmail.com>
2013-02-21 10:39:26 +0100

    FileChooser under construction

---
+++ src/UI/FileChooser.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Martin
+ */
+public class FileChooser extends JFrame{
+
+    private String path;
+    private JFrame frame;
+
+    public FileChooser(JFrame frame) {
+
+        this.frame = frame;
+    }
+
+    class OpenClass implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser chooser = new JFileChooser();
+
+            chooser.setAcceptAllFileFilterUsed(false);
+            chooser.setFileFilter(new MyCustomFilter());
+
+            int option = chooser.showOpenDialog(FileChooser.this);
+            if (option == JFileChooser.APPROVE_OPTION) {
+                path = chooser.getSelectedFile().getPath();
+            }
+        }
+    }
+
+    class MyCustomFilter extends javax.swing.filechooser.FileFilter {
+
+        @Override
+        public boolean accept(File file) {
+            // Allow only directories, or files with ".txt" extension
+            return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
+        }
+
+        @Override
+        public String getDescription() {
+            return "Text documents (*.txt)";
+        }
+    }
+
+    public String getAbsolutePath() {
+        return path;
+    }
+}
